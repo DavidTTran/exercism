f31c5829c0b7578bf2400eeb5b8d001599ecab31
David Tran <davidttemail@gmail.com>
2020-08-31 12:25:17 -0600

    Refactor

--- ruby/armstrong-numbers/armstrong_numbers.rb
+++ ruby/armstrong-numbers/armstrong_numbers.rb
@@ -1,10 +1,19 @@
 class ArmstrongNumbers
 
   def self.include?(integer)
-    numbers = integer.digits
-    sum = numbers.sum do |number|
-      number ** numbers.length
+    numbers = integer_to_digits(integer)
+    sum_after_exponent(numbers) == integer ? true : false
+  end
+
+  private
+
+  def self.integer_to_digits(integer)
+    integer.digits
+  end
+
+  def self.sum_after_exponent(integers)
+    integers.sum do |number|
+      number ** integers.length
     end
-    sum == integer ? true : false
   end
 end
